91d471f767963aaeaf53597c1f24aff69d524f36
mmoraes <mmoraes@uolinc.com>
2016-04-07 13:42:43 -0300

    ajustes

--- src/main/java/demorabbit/demos/demoapp/checkout/CheckoutStatus.java
+++
@@ -1,8 +0,0 @@
-package demorabbit.demos.demoapp.checkout;
-
-/**
- * Created by mmoraes on 2016-01-07.
- */
-public enum CheckoutStatus {
-    ORDERED, CREATED
-}
--- src/main/java/demorabbit/demos/demoapp/order/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package demorabbit.demos.demoapp.order;
-
-/**
- * Created by mmoraes on 2016-01-07.
- */
-public enum OrderStatus {
-    CREATED
-}
